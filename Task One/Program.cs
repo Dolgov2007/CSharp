6a35b8dc8b65888ef08181625e5ad8afdc31277d
Сенчик <arseniytyurin@mail.ru>
2025-09-25 22:49:03 +0300

    Task One

---
+++ Task One/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskOne
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Пожалуйста, введите температуру в градусах Цельсия: ");
+
+            if (int.TryParse(Console.ReadLine(), out int celsius))
+            {
+                int fahrenheit = (int)(celsius * 1.8) + 32; 
+                Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка ввода!");   
+            }
+        }
+    }
+}

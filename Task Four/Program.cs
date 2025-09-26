9cf8d4c93c303b57cb40e8cee8700047bf16a35b
Сенчик <arseniytyurin@mail.ru>
2025-09-26 21:03:58 +0300

    Task Four

---
+++ Task Four/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TaskFour
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Пожалуйста, введите пароль: ");
+
+            while (true)
+            {
+                string password = Console.ReadLine();
+                if (password.Length > 8)
+                {
+                    if (password.Any(char.IsDigit))
+                    {
+                        if (password.Any(char.IsUpper))
+                        {
+                            if (password.Any(c => !char.IsLetterOrDigit(c)))
+                            {
+                                Console.WriteLine("Поздравляем! Ваш пароль сильный!");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Нет специальных символов!");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("В Вашем пароле нет заглавных букв");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("В Вашем пароле нет цифр!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Длина меньше 8 символов!");
+                }
+            }
+        }
+    }
+}
